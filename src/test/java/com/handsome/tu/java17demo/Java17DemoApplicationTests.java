ffa7e097e6c95f3536bb2c43d2915069ed981e9e
Zhongxiu Tu <Zhongxiu.Tu@cicc.com.cn>
2022-03-30 10:20:58 +0800

    feat: first commit

---
+++ src/test/java/com/handsome/tu/java17demo/Java17DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handsome.tu.java17demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
